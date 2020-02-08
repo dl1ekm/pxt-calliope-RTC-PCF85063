3c0bc4357d6a6cace93224c24db1af73fcd81240
Raik <Raik.Andritschke@t-online.de>
2020-02-08 18:26:20 +0100

    Modifications

--- test.ts
+++ test.ts
@@ -1,1 +1,21 @@
-// tests go here; this will not be compiled when this package is used as a library
+let Uhrzeit = ""
+let Datum = ""
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_EVT_ANY, () => {
+    PCF85063TP.setClock(
+    2020,
+    2,
+    5,
+    7,
+    20,
+    2,
+    0
+    )
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_EVT_ANY, () => {
+    Datum = PCF85063TP.getDate()
+    Uhrzeit = PCF85063TP.getTime()
+    serial.writeLine("------------")
+    serial.writeLine(Datum)
+    serial.writeLine(Uhrzeit)
+    basic.pause(100)
+})
